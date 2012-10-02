5ce703c675cd41b77ddbd18a783cd72e66c993e1
Devin Carraway <git@devin.com>
2012-10-02 06:33:56 +0000

    Export GPIO lines if they're not already.
    On systems without customized udev, this may require
    root privs where previously one had chgrpp'ed around
    things -- hence no attempt is made to unexport after
    we're done.

--- gpio/gpio.go
+++ gpio/gpio.go
@@ -19,7 +19,11 @@ func NewGPIOLine(number uint, direction int) (gpio *GPIOLine, err error) {
 	gpio = new(GPIOLine)
 	gpio.Number = number
 
-	if err := gpio.SetDirection(direction); err != nil {
+	if err := gpio.enable_export(); err != nil {
+		return nil, err
+	}
+	err = gpio.SetDirection(direction)
+	if err != nil {
 		return nil, err
 	}
 	gpio.fd, err = os.OpenFile(fmt.Sprintf("/sys/class/gpio/gpio%d/value", gpio.Number), os.O_WRONLY|os.O_SYNC, 0666)
@@ -27,6 +31,23 @@ func NewGPIOLine(number uint, direction int) (gpio *GPIOLine, err error) {
 		return nil, err
 	}
 	return gpio, nil
+}
+
+func (gpio *GPIOLine) enable_export() error {
+	_, err := os.Stat(fmt.Sprintf("/sys/class/gpio/gpio%d", gpio.Number))
+	if err == nil {
+		// already exported
+		return nil
+	} else if err != nil && !os.IsNotExist(err) {
+		// some other error
+		return err
+	}
+	fd, err := os.OpenFile("/sys/class/gpio/export", os.O_WRONLY|os.O_SYNC, 0666)
+	if err != nil {
+		return err
+	}
+	_, err = fmt.Fprintf(fd, "%d\n", gpio.Number)
+	return err
 }
 
 func (gpio *GPIOLine) SetDirection(direction int) error {
