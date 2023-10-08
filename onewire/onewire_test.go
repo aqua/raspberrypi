8695637ef227d0b8cde27aae47a912f60ed90b97
Devin Carraway <git@devin.com>
2023-10-07 19:06:54 -0700

    also test failure

--- onewire/onewire_test.go
+++ onewire/onewire_test.go
@@ -13,7 +13,7 @@ func TestScan(t *testing.T) {
 		t.Errorf("got error in Scan(): %v", err)
 		return
 	}
-	want := []string{"28-000003d44061", "28-000003d45c8f"}
+	want := []string{"28-000003d44061", "28-000003d44bad", "28-000003d45c8f"}
 	sort.Strings(got)
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("Scan yielded %v, want %v", got, want)
