6bf07a2e9c82a6bec075f71dd541ba7b458b322c
Devin Carraway <git@devin.com>
2023-10-07 19:55:38 -0700

    fix Scan reference, use log

--- read1wire.go
+++ read1wire.go
@@ -4,9 +4,10 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"rpi/onewire"
+	"log"
 	"time"
+
+	"github.com/aqua/raspberrypi/onewire"
 )
 
 var id = flag.String("id", "", "Device to sample")
@@ -15,35 +16,35 @@ var delay = flag.Int64("delay", 5000, "Delay n msec between samples")
 func main() {
 	flag.Parse()
 
-	names, err := onewire.ScanSlaves()
+	names, err := onewire.Scan()
 	if err != nil {
-		fmt.Printf("Error scanning 1wire names: %v", err)
+		log.Printf("Error scanning 1wire names: %v", err)
 		return
 	}
-	fmt.Printf("scan = %v\n", names)
+	log.Printf("scan = %v\n", names)
 
 	devices := make([]*onewire.DS18S20, len(names))
 	for i := range names {
 		devices[i], err = onewire.NewDS18S20(names[i])
 		if err != nil {
-			fmt.Printf("Error opening device %v: %v", devices[i], err)
+			log.Printf("Error opening device %v: %v", devices[i], err)
 			return
 		}
 	}
 
 	if err != nil {
-		fmt.Printf("Error setting up 1wire slave %v: %v", *id, err)
+		log.Printf("Error setting up 1wire slave %v: %v", *id, err)
 		return
 	}
 
 	for {
 		for i := range devices {
-			fmt.Printf("attempting read on device %x\n", devices[i].Id)
+			log.Printf("attempting read on device %x\n", devices[i].Id)
 			value, err := devices[i].Read()
 			if err != nil {
-				fmt.Printf("Error on read: %v", err)
+				log.Printf("Error on read: %v", err)
 			} else {
-				fmt.Printf("Sample: %v°C\n", float64(value)/1000)
+				log.Printf("Sample: %v°C\n", float64(value)/1000)
 			}
 			time.Sleep(time.Duration(*delay) * time.Millisecond)
 		}
