603fed573e65ace480bf798bce61c86b96b20ea9
Devin Carraway <git@devin.com>
2023-10-07 18:12:40 -0700

    Reword, we're finding devices, not devices in a role

--- onewire/onewire.go
+++ onewire/onewire.go
@@ -5,8 +5,8 @@ import (
 	"strings"
 )
 
-/* Find all attached 1-wire slave devices */
-func ScanSlaves() ([]string, error) {
+/* Find all 1-wire devices attached to bus masters */
+func Scan() ([]string, error) {
 	devicedir, err := os.Open("/sys/bus/w1/devices/")
 	if err != nil {
 		return nil, err
@@ -18,7 +18,7 @@ func ScanSlaves() ([]string, error) {
 	}
 	r := make([]string, 0, len(names)-1)
 	for i := range names {
-		if ! strings.Contains(names[i], "w1_bus_master") {
+		if !strings.Contains(names[i], "w1_bus_master") {
 			r = append(r, names[i])
 		}
 	}
